6bba5c3e281b020488f784a049baf1e93c3dc97b
bdDavin <ben@davin.se>
2019-08-14 11:13:08 +0200

    Added fetch and some design to the store

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,25 +1,69 @@
-
 <template>
     <Page class="page">
-        <ActionBar title="Home" class="action-bar" />
-        <StackLayout>
+        <ActionBar title="YardSale" class="action-bar">
+        </ActionBar>
+        <GridLayout columns="*" rows="*, 90"> 
+          <ScrollView row="0" col="0">
+            <StackLayout id="backround">
+              <product-card :product="product" v-for="product in products" :key="product.id"></product-card>
+            </StackLayout>
+          </ScrollView>
+          <StackLayout row="1" col="0">
             <Button @tap="goToCart" text="Cart"></Button>
             <Button @tap="goToCheckout" text="Checkout"></Button>
             <Button @tap="goToProduct" text="Product"></Button>
-        </StackLayout>
+          </StackLayout>
+        </GridLayout>
     </Page>
 </template>
+
 <script>
-import Cart from './Cart.vue';
-import Checkout from './Checkout.vue';
-import Product from './Product.vue';
+import Cart from './Cart.vue'
+import Checkout from './Checkout.vue'
+import Product from './Product.vue'
+import ProductCard from './ProductCard.vue'
 
 export default {
+    created() {
+        this.getProductsFromDB(1)
+    },
+    components: {
+      ProductCard
+    },
     data () {
         return {
+          activeTab: 0,
+          activeLetter: 0,
+          currentPage: 1,
+          productsPerPage: 16,
+          products: [],
+          totalCount: 0
         };
     },
     methods: {
+        getProductsFromDB(payload) {
+          fetch('http://localhost:5000/api/products/?filter='+this.activeTab+'&letter='+this.activeLetter+'&page='+payload)
+          .then(response => response.json())
+          .then(result => {
+            if(result.length === 0) {
+              this.totalCount = 0
+            }else {
+              this.totalCount = result[0].full_count
+            }
+            this.products = result
+          }).catch(error => {
+              console.log(error.message)
+          })
+        },
+        tabChanged() {
+          this.currentPage = 1
+          this.getProductsFromDB(this.currentPage)
+        },
+        pageChanged(payload) {
+          this.getProductsFromDB(payload)
+          //Scroll to top of grid
+          window.scrollTo(0, 90)
+        },
         goToCart() {
             this.$navigateTo(Cart)
         },
@@ -34,18 +78,11 @@ export default {
 </script>
 
 <style scoped>
-</style>
-
-<style scoped>
     ActionBar {
         background-color: #53ba82;
         color: #ffffff;
     }
-
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
+    #backround {
+      background-color: #454d66
     }
 </style>
